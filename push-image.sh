b85554deda39c96a472d63735c73f35f169c867f
kwcloudconsutling <kwcloudconsulting@gmail.com>
2022-10-17 12:13:13 -0500

    change file

--- push-image.sh
+++ push-image.sh
@@ -4,10 +4,10 @@
 set -eu
 
 #login to docker hub
-sudo docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
+docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
 
 # use the docker tag command to add a tag to your image
-sudo docker tag $IMAGE_TAG $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
+docker tag $IMAGE_TAG $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
 
 # push the image to docker hub
-sudo docker push $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
+docker push $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
