6701a3c144ef1c2c6a2e532712b4271d456bad11
kwcloudconsutling <kwcloudconsulting@gmail.com>
2022-10-17 12:07:03 -0500

    debug

--- build-image.sh
+++ build-image.sh
@@ -4,4 +4,4 @@
 set -eu
 
 # build the docker image
-docker build -f $IMAGE_TAG/Dockerfile -t $IMAGE_TAG .
+sudo docker build -f $IMAGE_TAG/Dockerfile -t $IMAGE_TAG .
--- push-image.sh
+++ push-image.sh
@@ -4,10 +4,10 @@
 set -eu
 
 #login to docker hub
-docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
+sudo docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
 
 # use the docker tag command to add a tag to your image
-docker tag $IMAGE_TAG $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
+sudo docker tag $IMAGE_TAG $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
 
 # push the image to docker hub
-docker push $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
+sudo docker push $DOCKERHUB_USERNAME/$DOCKERHUB_REPONAME
